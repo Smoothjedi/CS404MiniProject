b44af2bc62e4f6d74b595262e95f361cb6867b88
Smoothjedi <xanolithogrania@gmail.com>
2023-10-24 21:25:01 -0500

    Add project files.

---
+++ MiniProject.SearchMethods/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+
+namespace MiniProject.SearchMethods
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var results = new List<string>() { "SortType, NumberOfElements, TimeInTicks" };
+            SearchMethods sm = new SearchMethods();
+            string directory = AppDomain.CurrentDomain.BaseDirectory;
+            var randomSeed = new Random();
+
+            for (int i = 10; i < 10000; i+=100)
+            {
+                var ints = new List<int>();
+                for (int j = 0; j < i; j++)
+                {
+                    var randomNumber = randomSeed.Next();
+                    ints.Add(randomNumber);
+                }
+                    var serializedList = JsonSerializer.Serialize(ints);
+
+                    var bubbleSortCopy = JsonSerializer.Deserialize<List<int>>(serializedList);
+                    results.Add(BubbleSort(sm, bubbleSortCopy));
+
+                    var quickSortCopy = JsonSerializer.Deserialize<List<int>>(serializedList);
+                    results.Add(QuickSort(sm, quickSortCopy));                
+            }
+            using (StreamWriter outputFile = 
+                new StreamWriter("Output.txt"))
+            {
+                foreach (string result in results)
+                    outputFile.WriteLine(result);
+            }
+        }
+
+        private static string BubbleSort(SearchMethods sm, List<int> listToSort)
+        {
+            if (listToSort == null) return string.Empty;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var bsResult = sm.BubbleSort(listToSort);
+            sw.Stop();
+            var result = $"BubbleSort, {listToSort.Count}, {sw.ElapsedTicks}";
+            sw.Reset();
+            return result;
+        }
+
+        private static string QuickSort(SearchMethods sm, List<int> listToSort)
+        {
+            if (listToSort == null) return string.Empty;
+            Stopwatch sw = new Stopwatch(); 
+            sw.Start();
+            var qsResult = sm.QuickSort(listToSort, 0, listToSort.Count - 1);
+            sw.Stop();
+            var result = $"QuickSort, {listToSort.Count}, {sw.ElapsedTicks}";
+            sw.Reset();
+            return result;
+        }
+    }
+}
---
+++ MiniProject.SearchMethods/SearchMethods.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+
+namespace MiniProject.SearchMethods
+{
+    public class SearchMethods
+    {
+        public List<int> BubbleSort(List<int> initialList)
+        {
+            var listCopy = JsonSerializer.Serialize(initialList);
+            var copiedList = JsonSerializer.Deserialize<List<int>>(listCopy);
+            if (copiedList == null)
+                return new List<int>();
+
+            for (int j = 0; j <= copiedList.Count - 2; j++)
+            {
+                for (int i = 0; i <= copiedList.Count - 2; i++)
+                {
+                    if (copiedList[i] > copiedList[i + 1])
+                    {
+                        copiedList.Reverse(i, 2);
+                    }
+                }
+            }
+            return copiedList;
+        }
+
+        public List<int> QuickSort(List<int> list, int left, int right)
+        {
+            var i = left;
+            var j = right;
+            var pivot = list[left];
+            while (i <= j)
+            {
+                while (list[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (list[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                QuickSort(list, left, j);
+            if (i < right)
+                QuickSort(list, i, right);
+            return list;
+        }
+    }
+}
